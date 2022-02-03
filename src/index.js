9688575db03a72fc1687f5276a537c159c1dda95
Ivo <ivo.iliev@vodafone.com>
2022-02-03 17:20:57 +0000

    remove deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async () => {
   // TODO implement
   const response = {
       statusCode: 200,
-      body: JSON.stringify('It worked!!'),
+      body: JSON.stringify('It really worked!!'),
   };
   return response;
 };
