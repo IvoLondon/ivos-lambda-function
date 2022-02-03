ceff3cfd5757d9afe0ac40dc250caddc2b75d859
Ivo <ivo.iliev@vodafone.com>
2022-02-03 16:53:02 +0000

    init commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const handler = async () => {
+  // TODO implement
+  const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello World!'),
+  };
+  return response;
+};
+export default handler;
